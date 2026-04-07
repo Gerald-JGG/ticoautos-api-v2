65e600c4744bf773c526b160af4ef66c86e1bb82
Gerald <gonzalezg375@gmail.com>
2026-04-07 12:23:59 -0600

    Refactor login method to return token alongside user object

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { UsersService } from '../users/users.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { LoginDto } from './dto/login.dto';
 import { CedulaService } from '../cedula/cedula.service';
+import type { UserDocument } from '../users/user.schema';
 
 @Injectable()
 export class AuthService {
@@ -24,12 +25,21 @@ export class AuthService {
   async login(loginDto: LoginDto) {
     const user = await this.usersService.findByEmail(loginDto.email);
     if (!user) throw new UnauthorizedException('Invalid credentials');
-
     const isPasswordValid = await bcrypt.compare(loginDto.password, user.password);
     if (!isPasswordValid) throw new UnauthorizedException('Invalid credentials');
-
     const token = this.generateToken(user._id.toString(), user.email);
     return { user, token };
+  }
+
+  async handleGoogleLogin(user: UserDocument) {
+    const token = this.generateToken(user._id.toString(), user.email);
+    return { user, token };
+  }
+
+  async completeGoogleProfile(userId: string, cedula: string, name: string) {
+    await this.cedulaService.validate(cedula);
+    const user = await this.usersService.updateCedula(userId, cedula, name);
+    return { user };
   }
 
   private generateToken(userId: string, email: string) {
