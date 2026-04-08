f0b5ead9f2f1a087cd262ac523dba0c8731516c2
Gerald <gonzalezg375@gmail.com>
2026-04-07 18:03:55 -0600

    Remove optional decorator from cedula field in CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -11,7 +11,6 @@ export class CreateUserDto {
   @MinLength(6, { message: 'Password must be at least 6 characters' })
   password: string;
 
-  @IsOptional()
   @IsString()
   @Matches(/^\d{9}$/, { message: 'Cédula must be exactly 9 digits' })
   cedula: string;
